48474a0c21ea31a2af85854502fc11dc0cd8771b
Alex Godfrey <alexjamesgodfrey@gmail.com>
2022-03-29 13:01:29 -0400

    initial front

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Test</h1>
+    </div>
+  )
+}
+
+export default Home
