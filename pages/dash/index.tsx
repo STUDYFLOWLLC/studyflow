24534aa8fdb0d1c7b942410ccb8e41de9c6d0eae
Alex Godfrey <alexjamesgodfrey@gmail.com>
2022-05-29 17:01:45 -0400

    updated skeleton theming for dashboard

--- pages/dash/index.tsx
+++ pages/dash/index.tsx
@@ -1,4 +1,5 @@
 import { User, withAuthRequired } from '@supabase/supabase-auth-helpers/nextjs'
+import classnames from 'classnames'
 import CMDPalette from 'components/CMDPalette'
 import DashBar from 'components/Dashbar'
 import DashHeadBig from 'components/Dashboard/DashHeadBig'
@@ -8,39 +9,53 @@ import DashHeadSmall from 'components/DashHeadSmall'
 import FlowTable from 'components/FlowTable'
 import Taskover from 'components/Taskover'
 import useUserDetails from 'hooks/useUserDetails'
+import { useTheme } from 'next-themes'
 import { useState } from 'react'
+import { SkeletonTheme } from 'react-loading-skeleton'
 
 interface Props {
   user: User
 }
 
 export default function Example({ user }: Props) {
-  const { userDetails, isLoading, isError } = useUserDetails(user.email)
+  const { theme } = useTheme()
 
+  const { userDetails, isLoading, isError } = useUserDetails(user.email)
   const [searchValue, setSearchValue] = useState('')
 
   if (isError) return <p>error</p>
   // if (userDetails === undefined) router.push('/setup')
 
   return (
-    <div className="min-h-full">
-      <DashBar
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-        loading={isLoading}
-      />
-      <div className="lg:pl-56 flex flex-col">
-        <DashHeadSmall />
-        <main className="flex-1">
-          <DashHeadBig />
-          <Pinned />
-          <FlowListSmall />
-          <FlowTable />
-        </main>
+    <SkeletonTheme
+      baseColor={classnames(
+        { '#ebebeb': theme === 'light' },
+        { '#202020': theme === 'dark' },
+      )}
+      highlightColor={classnames(
+        { '#f5f5f5': theme === 'light' },
+        { '#444': theme === 'dark' },
+      )}
+    >
+      <div className="min-h-full">
+        <DashBar
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+          loading={isLoading}
+        />
+        <div className="lg:pl-56 flex flex-col">
+          <DashHeadSmall />
+          <main className="flex-1">
+            <DashHeadBig />
+            <Pinned />
+            <FlowListSmall />
+            <FlowTable />
+          </main>
+        </div>
+        <Taskover />
+        <CMDPalette />
       </div>
-      <Taskover />
-      <CMDPalette />
-    </div>
+    </SkeletonTheme>
   )
 }
 
