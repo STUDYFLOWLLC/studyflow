e9446148dc6cd541be618ebe6c04b23f051d2ccf
Alex Godfrey <alexjamesgodfrey@gmail.com>
2022-05-31 19:26:05 -0400

    Componetize setup header

---
+++ components/Setup/SetupHeader.tsx
@@ -0,0 +1,10 @@
+export default function SetupHeader() {
+  return (
+    <div className="leading-4 w-full p-2 sm:p-4 sm:leading-5">
+      <div className="w-32">
+        <p className="text-2xl font-semibold">Studyflow</p>
+        <p className="text-right font-semibold">setup</p>
+      </div>
+    </div>
+  )
+}
