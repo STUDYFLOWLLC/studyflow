ba097979c54d4ff3b56d757e8ca79003365245de
STUDYFLOWLLC <102125072+STUDYFLOWLLC@users.noreply.github.com>
2022-05-31 13:49:49 -0400

    Fix/relative paths (#23)
    * made relative paths
    
    * fixed import order
    
    Co-authored-by: Alex Godfrey <alexjamesgodfrey@gmail.com>

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,11 +1,11 @@
 import 'reflect-metadata'
 import { ApolloServer } from 'apollo-server-micro'
 import { MicroRequest } from 'apollo-server-micro/dist/types'
-import { createContext } from '../../graphql/context'
 import { ServerResponse } from 'http'
 import Cors from 'micro-cors'
-import { resolvers } from '../../prisma/generated/type-graphql'
 import { buildSchema } from 'type-graphql'
+import { createContext } from '../../graphql/context'
+import { resolvers } from '../../prisma/generated/type-graphql'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const cors = new (Cors as any)({
