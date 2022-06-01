9e7b5f712e1e87818df39ab36e848b0ccd90e39d
Alex Godfrey <alexjamesgodfrey@gmail.com>
2022-06-01 08:03:19 -0400

    Add protected route

--- pages/setup/index.tsx
+++ pages/setup/index.tsx
@@ -1,15 +1,28 @@
+import { withPageAuth } from '@supabase/supabase-auth-helpers/nextjs'
 import classnames from 'classnames'
-import ClassSearch from 'components/Forms/ClassSearch'
 import SchoolSearch from 'components/Forms/SchoolSearch'
+import SetupHeader from 'components/Setup/SetupHeader'
 import { useTheme } from 'next-themes'
 import { useEffect, useState } from 'react'
+import { useHotkeys } from 'react-hotkeys-hook'
 import { SkeletonTheme } from 'react-loading-skeleton'
 
 export default function index() {
-  const { theme } = useTheme()
+  const { theme, setTheme } = useTheme()
 
   const [mounted, setMounted] = useState(false)
 
+  useHotkeys(
+    'cmd+l, ctrl+l',
+    (e) => {
+      e.preventDefault()
+      setTheme(theme === 'light' ? 'dark' : 'light')
+    },
+    {
+      enableOnTags: ['INPUT', 'TEXTAREA', 'SELECT'],
+    },
+    [theme],
+  )
   useEffect(() => setMounted(true), [])
 
   if (!mounted) return null
@@ -18,17 +31,19 @@ export default function index() {
     <SkeletonTheme
       baseColor={classnames(
         { '#ebebeb': theme === 'light' },
-        { '#202020': theme === 'dark' },
+        { '#313131': theme === 'dark' },
       )}
       highlightColor={classnames(
         { '#f5f5f5': theme === 'light' },
-        { '#444': theme === 'dark' },
+        { '#666': theme === 'dark' },
       )}
     >
       <div className="w-full flex flex-col items-center">
+        <SetupHeader />
         <SchoolSearch />
-        <ClassSearch />
       </div>
     </SkeletonTheme>
   )
 }
+
+export const getServerSideProps = withPageAuth({ redirectTo: '/login' })
