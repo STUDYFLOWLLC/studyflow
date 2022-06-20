171c6d3430870b461f9db842a5a5bf2862cedab6
alexjamesgodfrey <me@alexgodfrey.com>
2022-06-20 09:55:12 -0700

    Add userdetails fetching to bigprofile button

--- components/buttons/BigProfileButton.tsx
+++ components/buttons/BigProfileButton.tsx
@@ -1,6 +1,6 @@
 import { Menu, Transition } from '@headlessui/react'
 import { SelectorIcon } from '@heroicons/react/solid'
-import classnames from 'classnames'
+import { default as classNames, default as classnames } from 'classnames'
 import { useTheme } from 'next-themes'
 import { Fragment, useEffect, useState } from 'react'
 import Skeleton from 'react-loading-skeleton'
@@ -46,12 +46,25 @@ export default function BigProfileButton({
           <span className="flex w-full justify-between items-center">
             <span className="flex min-w-0 mr-3 items-center justify-between space-x-3">
               {!loading ? (
-                <img
-                  className="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0"
-                  src={pfpLink}
-                  // src="https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=256&h=256&q=80"
-                  alt=""
-                />
+                <div className="avatar placeholder online">
+                  <div
+                    className={classNames(
+                      { 'bg-stone-200': theme === 'light' },
+                      { 'bg-slate-700': theme === 'dark' },
+                      ' w-10 h-10 rounded-full flex-shrink-0',
+                    )}
+                  >
+                    {pfpLink ? (
+                      <img
+                        className="rounded-full flex-shrink-0"
+                        src={pfpLink}
+                        alt="the user's avatar"
+                      />
+                    ) : (
+                      <span className="text-lg sm:text-xl">SF</span>
+                    )}
+                  </div>
+                </div>
               ) : (
                 <Skeleton className="w-10 h-10" circle />
               )}
