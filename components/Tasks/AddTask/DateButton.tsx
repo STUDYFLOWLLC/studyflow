0332d1522ccf53390cdc8bfa547c3d7b7f1de3c0
sieapi <107765424+sieapi@users.noreply.github.com>
2022-06-23 13:08:34 -0700

    Sierra 001 (#73)
    * Style due date button
    
    * Style due date button

--- components/Tasks/AddTask/DateButton.tsx
+++ components/Tasks/AddTask/DateButton.tsx
@@ -1,3 +1,4 @@
+import { CalendarIcon } from '@heroicons/react/outline'
 import abbreviateDate from 'utils/abbreviateDate'
 
 interface Props {
@@ -11,7 +12,8 @@ export default function DateButton({
 }: Props) {
   console.log(taskDueDateExact)
   return (
-    <div>
+    <div className="flex items-center cursor-pointer hover:bg-gray-100 px-2 py-1 border border-gray-300 rounded-md shadow-sm mx-2 text-sm font-medium">
+      <CalendarIcon className="w-4 mr-2" />
       {taskDueDateExact
         ? abbreviateDate(taskDueDateExact, 'America/Los_Angeles')
         : 'Due date'}{' '}
