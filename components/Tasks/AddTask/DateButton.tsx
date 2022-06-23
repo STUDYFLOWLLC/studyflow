f9860814b40e109ec82f0ebfd06a9e79b85c1e94
sieapi <sieapi13@gmail.com>
2022-06-23 13:02:46 -0700

    Style due date button

--- components/Tasks/AddTask/DateButton.tsx
+++ components/Tasks/AddTask/DateButton.tsx
@@ -13,7 +13,7 @@ export default function DateButton({
   console.log(taskDueDateExact)
   return (
     <div className="flex items-center cursor-pointer hover:bg-gray-100 px-2 py-1 border border-gray-300 rounded-md shadow-sm mx-2 text-sm font-medium">
-      <CalendarIcon className="w-4 mr-2"/>
+      <CalendarIcon className="w-4 mr-2" />
       {taskDueDateExact
         ? abbreviateDate(taskDueDateExact, 'America/Los_Angeles')
         : 'Due date'}{' '}
