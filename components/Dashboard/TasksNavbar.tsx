181b41b64de853b598d7e5b9fbeca6049daf8ad9
sieapi <sieapi13@gmail.com>
2022-06-26 13:29:00 -0700

    Darkify task header

--- components/Dashboard/TasksNavbar.tsx
+++ components/Dashboard/TasksNavbar.tsx
@@ -4,6 +4,8 @@ import {
   FolderIcon,
 } from '@heroicons/react/outline'
 import classNames from 'classnames'
+import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
 
 const tasksDisplays = ['Today', 'Calendar', 'Courses']
 const tasksDisplaysIcons = [
@@ -18,6 +20,14 @@ interface Props {
 }
 
 export default function TasksNavbar({ taskView, setTaskView }: Props) {
+  const { theme } = useTheme()
+
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => setMounted(true), [])
+
+  if (!mounted) return null
+
   return (
     <div className="absolute bottom-0">
       <div className="flex ml-14">
@@ -28,11 +38,19 @@ export default function TasksNavbar({ taskView, setTaskView }: Props) {
             className={classNames(
               {
                 'border-b-2 border-primary text-black font-medium':
-                  taskDisplay === taskView,
+                  taskDisplay === taskView && theme === 'light',
               },
               {
                 'hover:cursor-pointer hover:text-black text-gray-500 hover:border-b-2 hover:border-gray-300':
-                  taskDisplay !== taskView,
+                  taskDisplay !== taskView && theme === 'light',
+              },
+              {
+                'border-b-2 border-primary font-medium text-gray-100':
+                  taskDisplay === taskView && theme === 'dark',
+              },
+              {
+                'hover:cursor-pointer hover:text-default hover:border-b-2 hover:border-gray-300':
+                  taskDisplay !== taskView && theme === 'dark',
               },
               'flex mx-3 pb-4 items-center',
             )}
