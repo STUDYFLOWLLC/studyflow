65c60c4629a6d226de84cb27e4b6d7b06dd2d430
Charles Mitz <cwmitz@gmail.com>
2022-06-27 12:58:42 -0700

    finished delete-task

--- components/Tasks/DisplayTasks/DeleteTask.tsx
+++ components/Tasks/DisplayTasks/DeleteTask.tsx
@@ -4,15 +4,12 @@ import { useUser } from '@supabase/supabase-auth-helpers/react'
 import deleteTask from 'hooks/tasks/deleteTask'
 import useTasks, { Task } from 'hooks/tasks/useTasks'
 import useUserDetails from 'hooks/useUserDetails'
-import { useState } from 'react'
 
 interface Props {
   task: Task
 }
 
 export default function DeleteTask({ task }: Props) {
-  const [deletePrompt, setDeletePrompt] = useState(false)
-
   const user = useUser()
   const { userDetails } = useUserDetails(user.user?.id)
   const { tasks, mutateTasks } = useTasks(userDetails?.UserID)
@@ -28,30 +25,17 @@ export default function DeleteTask({ task }: Props) {
       },
     )
 
-    // Reset local state
-    setDeletePrompt(false)
-
     // Delete remotely
     deleteTask(task.TaskID)
   }
 
   return (
-    <>
-      {!deletePrompt && (
-        <TrashIcon
-          onClick={() => {
-            deleteTasks()
-          }}
-          onKeyDown={() => deleteTasks()}
-          className="w-5 h-5 text-gray-400 hover:text-black hover:cursor-pointer"
-        />
-      )}
-      {deletePrompt && (
-        <div className="border px-1 items-center flex">
-          <span className="border mr-1.5">Delete</span>
-          <span>Cancel</span>
-        </div>
-      )}
-    </>
+    <TrashIcon
+      onClick={() => {
+        deleteTasks()
+      }}
+      onKeyDown={() => deleteTasks()}
+      className="w-5 h-5 text-gray-400 hover:text-black hover:cursor-pointer"
+    />
   )
 }
