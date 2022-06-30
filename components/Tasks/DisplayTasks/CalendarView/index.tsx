1f535435e126db1ee51c7c312fb1387b1927e672
Charles Mitz <cwmitz@gmail.com>
2022-06-30 10:56:39 -0700

    Start of TaskCalendar

--- components/Tasks/DisplayTasks/CalendarView/index.tsx
+++ components/Tasks/DisplayTasks/CalendarView/index.tsx
@@ -1,11 +1,14 @@
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid'
+import { User } from '@supabase/supabase-auth-helpers/nextjs'
 import classNames from 'classnames'
+import AddTask from 'components/Tasks/AddTask'
 import {
   add,
   eachDayOfInterval,
   endOfMonth,
   endOfWeek,
   format,
+  formatISO,
   getDay,
   isSameMonth,
   isToday,
@@ -14,7 +17,11 @@ import {
   startOfToday,
   startOfWeek,
 } from 'date-fns'
+import { CourseOnTerm } from 'hooks/school/useCoursesOnTerm'
+import { Task } from 'hooks/tasks/useTasks'
 import React from 'react'
+import { KeyedMutator } from 'swr'
+import BasicDisplayTasks from '../BasicDisplayTasks'
 
 const colStartClasses = [
   '',
@@ -27,8 +34,12 @@ const colStartClasses = [
 ]
 
 interface Props {
-  taskDueDateExact: Date | undefined
-  setTaskDueDateExact: (taskDueDateExact: Date | undefined) => void
+  tasks: Task[]
+  archiveTaskLocal: (TaskID: number) => void
+  user: User
+  coursesOnTerm: CourseOnTerm[]
+  coursesOnTermLoading: boolean
+  mutateTasks: KeyedMutator<any>
 }
 
 const sameDate = (date1: Date | undefined, date2: Date | undefined) => {
@@ -40,33 +51,90 @@ const sameDate = (date1: Date | undefined, date2: Date | undefined) => {
   )
 }
 
-export default function DropdownCalendar({
-  taskDueDateExact,
-  setTaskDueDateExact,
+export default function CalendarView({
+  tasks,
+  archiveTaskLocal,
+  user,
+  coursesOnTerm,
+  coursesOnTermLoading,
+  mutateTasks,
 }: Props) {
+  // Today's date
   const today = startOfToday()
+
+  // Current month
   const [currentMonth, setCurrentMonth] = React.useState(
     format(today, 'MMM-yyyy'),
   )
+  // State of date that will show tasks displayed
+  const [dateToDisplay, setDateToDisplay] = React.useState(today)
+
+  // First day of current month
   const firstDayCurrentMonth = parse(currentMonth, 'MMM-yyyy', new Date())
 
+  // Days of current month
   const days = eachDayOfInterval({
     start: startOfWeek(startOfMonth(firstDayCurrentMonth)),
     end: endOfWeek(endOfMonth(firstDayCurrentMonth)),
   })
 
+  // Previous month
   const previousMonth = () => {
     const firstDayLastMonth = add(firstDayCurrentMonth, { months: -1 })
     setCurrentMonth(format(firstDayLastMonth, 'MMM-yyyy'))
   }
 
+  // Next month
   const nextMonth = () => {
     const firstDayNextMonth = add(firstDayCurrentMonth, { months: 1 })
     setCurrentMonth(format(firstDayNextMonth, 'MMM-yyyy'))
   }
 
+  const numTasks = tasks.filter(
+    (task) =>
+      !task.Completed &&
+      task.DueDate.substring(0, 10) === formatISO(today).substring(0, 10),
+  ).length
+
   return (
-    <div className="w-64">
+    <div className="flex">
+      <div>
+        {/* TODO: Add header */}
+        <div>
+          <span className="mt-4 text-xl mr-2">
+            {dateToDisplay.toDateString().substring(0, 10)}
+          </span>
+          <span className="text-sm text-gray-400">
+            {numTasks} {numTasks === 1 ? 'Task' : 'Tasks'}
+          </span>
+        </div>
+
+        {/* Display Task with Viewing Date */}
+        <div>
+          <BasicDisplayTasks
+            archiveTaskLocal={archiveTaskLocal}
+            tasks={tasks.filter(
+              (task) =>
+                !task.Completed &&
+                task.DueDate.substring(0, 10) ===
+                  formatISO(dateToDisplay).substring(0, 10),
+            )}
+          />
+        </div>
+        {/* Add Task
+          TODO: Make it so the task you add has DueDate of dateToDisplay */}
+        <div>
+          <AddTask
+            user={user}
+            tasks={tasks}
+            mutateTasks={mutateTasks}
+            coursesOnTerm={coursesOnTerm}
+            coursesOnTermLoading={coursesOnTermLoading}
+          />
+        </div>
+      </div>
+
+      {/* Calendar Display */}
       <div className="md:grid md:grid-cols-2 md:divide-x md:divide-gray-200">
         <div className="w-64">
           <div className="flex items-center">
@@ -111,28 +179,28 @@ export default function DropdownCalendar({
               >
                 <button
                   type="button"
-                  onClick={() => setTaskDueDateExact(day)}
+                  onClick={() => setDateToDisplay(day)}
                   className={classNames(
-                    sameDate(day, taskDueDateExact) && 'text-white',
-                    !sameDate(day, taskDueDateExact) &&
+                    sameDate(day, dateToDisplay) && 'text-white',
+                    !sameDate(day, dateToDisplay) &&
                       isToday(day) &&
                       'text-primary',
-                    !sameDate(day, taskDueDateExact) &&
+                    !sameDate(day, dateToDisplay) &&
                       !isToday(day) &&
                       isSameMonth(day, firstDayCurrentMonth) &&
                       'text-gray-900',
-                    !sameDate(day, taskDueDateExact) &&
+                    !sameDate(day, dateToDisplay) &&
                       !isToday(day) &&
                       !isSameMonth(day, firstDayCurrentMonth) &&
                       'text-gray-400',
-                    sameDate(day, taskDueDateExact) &&
+                    sameDate(day, dateToDisplay) &&
                       isToday(day) &&
                       'bg-primary/70',
-                    sameDate(day, taskDueDateExact) &&
+                    sameDate(day, dateToDisplay) &&
                       !isToday(day) &&
                       'bg-gray-900',
-                    !sameDate(day, taskDueDateExact) && 'hover:bg-gray-200',
-                    (sameDate(day, taskDueDateExact) || isToday(day)) &&
+                    !sameDate(day, dateToDisplay) && 'hover:bg-gray-200',
+                    (sameDate(day, dateToDisplay) || isToday(day)) &&
                       'font-semibold',
                     'mx-auto flex h-8 w-8 items-center justify-center rounded-full',
                   )}
--- components/Tasks/DisplayTasks/TodayView.tsx
+++ components/Tasks/DisplayTasks/TodayView.tsx
@@ -1,9 +1,9 @@
 import { User } from '@supabase/supabase-auth-helpers/nextjs'
+import AddTask from 'components/Tasks/AddTask'
 import { CourseOnTerm } from 'hooks/school/useCoursesOnTerm'
 import { Task } from 'hooks/tasks/useTasks'
 import { KeyedMutator } from 'swr'
 import isToday from 'utils/isToday'
-import AddTask from '../AddTask'
 import BasicDisplayTasks from './BasicDisplayTasks'
 
 interface Props {
@@ -26,11 +26,17 @@ export default function TodayView({
   taskView,
 }: Props) {
   const today = new Date().toDateString().slice(0, 10)
-  const numTasksToday = tasks.length
+  const numTasksToday = tasks.filter(
+    (task) => !task.Completed && isToday(task),
+  ).length
+  console.log(new Date())
+  console.log(new Date().toDateString())
+  console.log(new Date().toISOString())
 
   return (
     <div className="w-8/12 flex flex-col">
       <div className="mt-5 mb-1">
+        {/* Header, which is date and num of tasks */}
         <span className="mt-4 text-xl mr-2">{today}</span>
         <span className="text-sm text-gray-400">
           {numTasksToday} {numTasksToday === 1 ? 'Task' : 'Tasks'}
