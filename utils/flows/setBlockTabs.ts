86def81ff2466f0edf45d8b3fbcac4d9aab77ea4
alexjamesgodfrey <me@alexgodfrey.com>
2022-07-03 13:37:08 -0700

    Document

--- utils/flows/setBlockTabs.ts
+++ utils/flows/setBlockTabs.ts
@@ -1,9 +1,19 @@
 import { Block } from 'types/Flow'
 
+/**
+ * Increment the number of tabs in a block (max 4)
+ * @param block The block to increment the tabs on
+ * @returns the number of tabs on the block after the increment
+ */
 export function incrementBlockTabs(block: Block) {
   return Math.min(block.tabs + 1, 4)
 }
 
+/**
+ * Decrement the number of tabs in a block (min 0)
+ * @param block The block to decrement the tabs on
+ * @returns the number of tabs on the block after the decrement
+ */
 export function decrementBlockTabs(block: Block) {
   return Math.max(block.tabs - 1, 0)
 }
