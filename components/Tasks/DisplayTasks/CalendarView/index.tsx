47cb85dfd6914cf71d4ccc55f9216d1139cc6137
Charles Mitz <101024748+cwmitz@users.noreply.github.com>
2022-07-11 17:27:17 -0700

    Calendar view editing 2 (#153)
    * json package
    
    * npm
    
    * other shit
    
    * lots of changes to the calendar
    
    * one more minor change to numtasks

--- components/Tasks/DisplayTasks/CalendarView/index.tsx
+++ components/Tasks/DisplayTasks/CalendarView/index.tsx
@@ -7,12 +7,14 @@ import {
   endOfMonth,
   endOfWeek,
   format,
+  isAfter,
   isSameMonth,
   isToday,
   parse,
   startOfMonth,
   startOfToday,
   startOfWeek,
+  startOfYesterday,
 } from 'date-fns'
 import useTasks from 'hooks/tasks/useTasks'
 import useUserDetails from 'hooks/useUserDetails'
@@ -39,32 +41,28 @@ export default function CalendarView({ user }: Props) {
   const { userDetails } = useUserDetails(user.id)
   const { tasks } = useTasks(userDetails?.UserID)
 
-  // Number of tasks Due on a given date
-  const numDueTasks = (date: Date) =>
-    tasks.filter(
+  // Number of tasks for each due-type on a given date. The completed if before the current date.
+  const numDueTasksByType = (date: Date, type?: TaskType, past?: boolean) => {
+    if (past) {
+      return tasks.filter(
+        (task) =>
+          task.DueDate?.substring(0, 10) ===
+          format(date, 'yyyy-MM-dd').substring(0, 10),
+      ).length
+    }
+    if (!type)
+      return tasks.filter(
+        (task) =>
+          !task.Completed &&
+          task.DueDate?.substring(0, 10) === format(date, 'yyyy-MM-dd'),
+      ).length
+    return tasks.filter(
       (task) =>
         !task.Completed &&
         task.DueDate?.substring(0, 10) === format(date, 'yyyy-MM-dd') &&
-        task.Type === TaskType.DUE,
+        task.Type === type,
     ).length
-
-  // Number of tasks Work-on on a given date
-  const numWorkOnTasks = (date: Date) =>
-    tasks.filter(
-      (task) =>
-        !task.Completed &&
-        task.DueDate?.substring(0, 10) === format(date, 'yyyy-MM-dd') &&
-        task.Type === TaskType.WORK_ON,
-    ).length
-
-  // Number of tasks Review on a given date
-  const numReviewTasks = (date: Date) =>
-    tasks.filter(
-      (task) =>
-        !task.Completed &&
-        task.DueDate?.substring(0, 10) === format(date, 'yyyy-MM-dd') &&
-        task.Type === TaskType.REVIEW,
-    ).length
+  }
 
   // Today's date
   const today = startOfToday()
@@ -101,35 +99,33 @@ export default function CalendarView({ user }: Props) {
     <div className="flex">
       {/* Calendar Display */}
       <div className="grid gird-cols-2 w-full my-6 mx-4 h-full">
-        <div className="flex justify-center mb-4">
+        <div className="flex justify-left mb-2">
+          <h2 className="text-2xl font-semibold text-gray-900 mr-4">
+            {format(firstDayCurrentMonth, 'MMM yyyy')}
+          </h2>
           <button
             onClick={previousMonth}
             type="button"
-            className="text-gray-400 hover:text-gray-800"
+            className="text-gray-400 hover:text-gray-800 mr-4"
           >
-            <span className="sr-only">Previous month</span>
             <ChevronLeftIcon className="h-7 w-7" aria-hidden="true" />
           </button>
-          <h2 className="text-2xl font-semibold text-gray-900 mx-6">
-            {format(firstDayCurrentMonth, 'MMM yyyy')}
-          </h2>
           <button
             onClick={nextMonth}
             type="button"
             className="text-gray-400 hover:text-gray-800"
           >
-            <span className="sr-only">Next month</span>
             <ChevronRightIcon className="h-7 w-7" aria-hidden="true" />
           </button>
         </div>
         <div className="isolate grid grid-cols-7 text-center text-xl text-gray-500 mb-2">
-          <div>S</div>
-          <div>M</div>
-          <div>T</div>
-          <div>W</div>
-          <div>T</div>
-          <div>F</div>
-          <div>S</div>
+          <div>Sun</div>
+          <div>Mon</div>
+          <div>Tue</div>
+          <div>Wed</div>
+          <div>Thu</div>
+          <div>Fri</div>
+          <div>Sat</div>
         </div>
         <div className="grid grid-cols-7 text-xl border-b border-l">
           {days.map((day) => (
@@ -137,57 +133,72 @@ export default function CalendarView({ user }: Props) {
               key={day.toString()}
               className="flex flex-col border-t border-r h-20"
             >
-              <button
-                type="button"
-                onClick={() => {
-                  setDateToDisplay(day)
-                  setOpen(true)
-                }}
-                className={classNames(
-                  sameDate(day, dateToDisplay) && 'text-white',
-                  !sameDate(day, dateToDisplay) &&
-                    isToday(day) &&
-                    'text-primary',
-                  !sameDate(day, dateToDisplay) &&
-                    !isToday(day) &&
-                    isSameMonth(day, firstDayCurrentMonth) &&
-                    'text-gray-900',
-                  !sameDate(day, dateToDisplay) &&
-                    !isToday(day) &&
-                    !isSameMonth(day, firstDayCurrentMonth) &&
-                    'text-gray-400',
-                  sameDate(day, dateToDisplay) &&
-                    isToday(day) &&
-                    'bg-primary/70',
-                  sameDate(day, dateToDisplay) &&
-                    !isToday(day) &&
-                    'bg-gray-900',
-                  !sameDate(day, dateToDisplay) && 'hover:bg-gray-200',
-                  (sameDate(day, dateToDisplay) || isToday(day)) &&
-                    'font-semibold',
-                  'h-7 w-7 mt-0.5 ml-0.5 text-sm border border-transparent rounded-full p-0.5',
-                )}
-              >
-                <time dateTime={format(day, 'yyyy-MM-dd')}>
-                  {format(day, 'd')}
-                </time>
-              </button>
-              <div className="text-xs ml-2">
-                {numWorkOnTasks(day) > 0 && (
-                  <div className="text-cyan-500">
-                    {numWorkOnTasks(day)} Work-on
-                  </div>
-                )}
-                {numDueTasks(day) > 0 && (
-                  <div className="text-orange-500">{numDueTasks(day)} Due</div>
-                )}
-                {numReviewTasks(day) > 0 && (
-                  <div className="text-purple-500">
-                    {numReviewTasks(day)}{' '}
-                    {numReviewTasks(day) > 1 ? 'Reviews' : 'Review'}
-                  </div>
-                )}
+              <div className="flex justify-center">
+                <button
+                  type="button"
+                  onClick={() => {
+                    setDateToDisplay(day)
+                    setOpen(true)
+                  }}
+                  className={classNames(
+                    sameDate(day, dateToDisplay) && 'text-white',
+                    !sameDate(day, dateToDisplay) &&
+                      isToday(day) &&
+                      'text-primary',
+                    !sameDate(day, dateToDisplay) &&
+                      !isToday(day) &&
+                      isSameMonth(day, firstDayCurrentMonth) &&
+                      'text-gray-900',
+                    !sameDate(day, dateToDisplay) &&
+                      !isToday(day) &&
+                      !isSameMonth(day, firstDayCurrentMonth) &&
+                      'text-gray-400',
+                    sameDate(day, dateToDisplay) &&
+                      isToday(day) &&
+                      'bg-primary/70',
+                    sameDate(day, dateToDisplay) &&
+                      !isToday(day) &&
+                      'bg-gray-900',
+                    !sameDate(day, dateToDisplay) && 'hover:bg-gray-200',
+                    (sameDate(day, dateToDisplay) || isToday(day)) &&
+                      'font-semibold',
+                    'h-6 w-6 mt-0.5 ml-0.5 text-sm border border-transparent rounded-full',
+                  )}
+                >
+                  <time dateTime={format(day, 'yyyy-MM-dd')}>
+                    {format(day, 'd')}
+                  </time>
+                </button>
               </div>
+
+              {/* Number of total tasks and tasks due. If they are before the current
+              date, it shows the number of completed tasks */}
+              {isAfter(day, startOfYesterday()) ? (
+                <div className="text-xs ml-2 mt-1">
+                  {numDueTasksByType(day) > 0 && (
+                    <div className="text-slate-900">
+                      {numDueTasksByType(day)}{' '}
+                      {numDueTasksByType(day) > 1 ? 'Tasks' : 'Task'}
+                    </div>
+                  )}
+                  {numDueTasksByType(day, TaskType.DUE) > 0 && (
+                    <div className="text-orange-500">
+                      {numDueTasksByType(day, TaskType.DUE)} Due
+                    </div>
+                  )}
+                </div>
+              ) : (
+                <div className="text-xs ml-2 mt-1 opacity-40">
+                  {numDueTasksByType(day, undefined, true) > 0 && (
+                    <div className="text-slate-900">
+                      {numDueTasksByType(day, undefined, true)}{' '}
+                      {numDueTasksByType(day, undefined, true) > 1
+                        ? 'Tasks'
+                        : 'Task'}
+                    </div>
+                  )}
+                </div>
+              )}
             </div>
           ))}
         </div>
