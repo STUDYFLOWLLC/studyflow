daee479e68f7a882adc9f5ece2cbe70cb837629b
Charles Mitz <cwmitz@gmail.com>
2022-07-18 14:44:08 -0700

    terms of service agreement

--- pages/termsofservice/index.tsx
+++ pages/termsofservice/index.tsx
@@ -1,3 +1,150 @@
 export default function index() {
-  return <div>index</div>
+  return (
+    <div className="prose mx-auto">
+      <h1>Terms of Service</h1>
+      <p>
+        These Website Standard Terms and Conditions contained herein on this
+        webpage, shall govern your use of this website, including all pages
+        within this website (collectively referred to herein below as this
+        “Website”). These Terms apply in full force and effect to your use of
+        this Website and by using this Website, you expressly accept all terms
+        and conditions contained herein in full. You must not use this Website,
+        if you have any objection to any of these Website Standard Terms and
+        Conditions.
+      </p>
+
+      <h2>Intellectual Property Rights</h2>
+      <p>
+        Other than content you own, which you may have opted to include on this
+        Website, under these Terms, Studyflow and/or its licensors own all
+        rights to the intellectual property and material contained in this
+        Website, and all such rights are reserved. You are granted a limited
+        license only, subject to the restrictions provided in these Terms, for
+        purposes of viewing the material contained on this Website.
+      </p>
+
+      <h2>Restrictions</h2>
+      <p>
+        You are expressly and emphatically restricted from all of the following:
+      </p>
+      <ul>
+        <li>Publishing any Website material in any media.</li>
+        <li>
+          Selling, sublicensing, and/or otherwise commercializing any Website
+          material.
+        </li>
+        <li>
+          Using this Website in any way that is, or may be, damaging to this
+          Website.
+        </li>
+        <li>
+          Using this Website in a way that impacts user access to this Website.
+        </li>
+        <li>
+          Using this Website contrary to applicable laws and regulations, or in
+          a way that causes, or may cause, harm to the Website, or to any person
+          or business entity.
+        </li>
+        <li>
+          Engaging in any data mining, data harvesting, data extracting or any
+          other similar activity in relation to this Website, or while using
+          this Website.
+        </li>
+        <li>Using this Website to engage in any advertising or marketing.</li>
+      </ul>
+      <p>
+        Certain areas of this Website are restricted from access by you and
+        Studyflow may further restrict any areas of this Website, at any time,
+        in its sole and absolute discretion. Any user ID and password you may
+        have for this Website are confidential and you must maintain
+        confidentiality of such information.
+      </p>
+
+      <h2>Your Content</h2>
+      <p>
+        In these Website Standard Terms and Conditions, “Your Content” shall
+        mean any audio, video, text, images or other material you choose to
+        display on this Website. With respect to Your Content, by displaying it,
+        you grant Studyflow a non-exclusive, worldwide irrevocable,
+        royalty-free, sublicensable license to use, reproduce, publish,
+        translate, and distribute it in aunty and all media.
+      </p>
+      <p>
+        Your content must be your own and must not be infringing on any third
+        party’s rights. Studyflow reserves the right to remove any of Your
+        Content from this Website at any time, and for any reason, without
+        notice.
+      </p>
+
+      <h2>No Warranties</h2>
+      <p>
+        This Website is provided “as is,” with all faults, and Studyflow makes
+        no express or implied representations or warranties, of any kind related
+        to this Website or the materials contained on this Website.
+        Additionally, nothing on this Website shall be construed as providing
+        consultation or advice to you.
+      </p>
+
+      <h2>Limitation of Liability</h2>
+      <p>
+        In no event shall Studyflow, nor any of its officers, directors, and
+        employees, be liable to you for anything arising out of or in any way
+        connected with your use of this Website, whether such liability is under
+        contract, tort or otherwise, and Studyflow, including its officers,
+        directors and employees shall not be liable for any indirect,
+        consequential or special liability arising out of or in any way related
+        to your use of this Website.
+      </p>
+
+      <h2>Indemnification</h2>
+      <p>
+        You hereby indemnify to the fullest extent Studyflow from and against
+        any and all liabilities, costs, demands, causes of action, damages and
+        expenses (including reasonable attorney’s fees) arising out of or in any
+        way related to your breach of any of the provisions of these Terms.
+      </p>
+
+      <h2>Severability</h2>
+      <p>
+        If any provision of these Terms is found to be unenforceable or invalid
+        under any applicable law, such unenforceability or invalidity shall not
+        render these terms unenforceable or invalid as a whole, and such
+        provisions shall be deleted without affecting the remaining provisions
+        herein.
+      </p>
+
+      <h2>Variation of Terms</h2>
+      <p>
+        Studyflow is permitted to revise these Terms at any time as it sees fit,
+        and by using this Website you are expected to review such Terms on a
+        regular basis to ensure you understand all terms and conditions
+        governing use of this Website.
+      </p>
+
+      <h2>Assignment</h2>
+      <p>
+        Studyflow shall be permitted to assign, translate, and subcontract its
+        rights and/or obligations under these Terms without any notification or
+        consent required. However, you shall not be permitted to assign,
+        transfer, or subcontract any of your rights and/or obligations under
+        these terms.
+      </p>
+
+      <h2>Entire Agreement</h2>
+      <p>
+        These Terms, including any legal notices and disclaimers contained on
+        this website, constitute the entire agreement between Studyflow and you
+        in relation to your use of this Website, and supersede all prior
+        agreements and understandings with respect to the same.
+      </p>
+
+      <h2>Governing Law and Jurisdiction</h2>
+      <p>
+        These Terms will be governed by and construed in accordance with the
+        laws of the State of New York, and you submit to the non-exclusive
+        jurisdiction of the state and federal courts located in New York for the
+        resolution of any disputes.
+      </p>
+    </div>
+  )
 }
