a951224db659d01e3b2b5779335abc9e7b05648f
Alex Godfrey <42598293+alexjamesgodfrey@users.noreply.github.com>
2022-07-20 18:21:30 -0400

    Charles setup (#171)

--- hooks/school/useCoursesOnTerm.ts
+++ hooks/school/useCoursesOnTerm.ts
@@ -29,7 +29,10 @@ interface Ret {
   mutateCoursesOnTerm: KeyedMutator<any>
 }
 
-export default function useCoursesOnTerm(termID: number | undefined): Ret {
+export default function useCoursesOnTerm(
+  termID: number | undefined,
+  filterFunk?: (arg0: CourseOnTerm) => boolean,
+): Ret {
   const query = gql`
     query Query($where: TermWhereInput) {
       findFirstTerm(where: $where) {
@@ -80,9 +83,9 @@ export default function useCoursesOnTerm(termID: number | undefined): Ret {
     data.findFirstTerm.FK_CourseOnTerm.length !== 0
   ) {
     return {
-      coursesOnTerm: data.findFirstTerm.FK_CourseOnTerm.sort(
-        (a: CourseOnTerm, b: CourseOnTerm) => (a.Index > b.Index ? 1 : -1),
-      ),
+      coursesOnTerm: !filterFunk
+        ? data.findFirstTerm.FK_CourseOnTerm
+        : data.findFirstTerm.FK_CourseOnTerm.filter(filterFunk),
       coursesOnTermLoading: false,
       coursesOnTermError: error,
       mutateCoursesOnTerm: mutate,
