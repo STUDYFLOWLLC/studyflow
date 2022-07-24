7f102e95a1d604bb8c8fd20eda58a7a33459ca12
alexjamesgodfrey <me@alexgodfrey.com>
2022-07-24 11:00:42 -0400

    Add error handling and automation icon

--- components/Automation/AutomationCardBody.tsx
+++ components/Automation/AutomationCardBody.tsx
@@ -2,14 +2,16 @@ import { CourseOnTermAutomation } from 'hooks/automation/useAutomationDetails'
 import LogLine from './LogLine'
 
 interface Props {
-  automation: CourseOnTermAutomation
+  automation: CourseOnTermAutomation | undefined
 }
 
 export default function AutomationCardBody({ automation }: Props) {
+  if (!automation) return <div />
+
   return (
     <div className="w-full flex flex-col items-center">
-      {automation.AutomationLog.length > 0 ? (
-        automation.AutomationLog.map((log) => (
+      {automation?.AutomationLog?.length > 0 ? (
+        automation?.AutomationLog.map((log) => (
           <LogLine key={log.AutomationLogID} log={log} />
         ))
       ) : (
