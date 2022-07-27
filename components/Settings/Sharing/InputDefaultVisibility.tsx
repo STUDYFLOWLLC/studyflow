8d532be74ba1c6cedafc79ad112a7105ade41804
alexjamesgodfrey <me@alexgodfrey.com>
2022-07-27 12:37:11 -0400

    Mutate default visibility

--- components/Settings/Sharing/InputDefaultVisibility.tsx
+++ components/Settings/Sharing/InputDefaultVisibility.tsx
@@ -8,11 +8,26 @@ import { useTheme } from 'next-themes'
 import { Fragment, useEffect, useState } from 'react'
 import { ActiveProps } from 'types/ActiveProps'
 import { FlowVisibility } from 'types/Flow'
+import { changeUserDefaultVisibility } from 'utils/user/userHandlers'
 
 const items = [
-  { name: FlowVisibility.PUBLIC, icon: RainbowPublicIcon },
-  { name: FlowVisibility.PRIVATE, icon: LockClosedIcon },
-  { name: FlowVisibility.HIDDEN, icon: BanIcon },
+  {
+    name: FlowVisibility.PUBLIC,
+    icon: RainbowPublicIcon,
+    description: 'By default, your flows will be shared publicly.',
+  },
+  {
+    name: FlowVisibility.PRIVATE,
+    icon: LockClosedIcon,
+    description:
+      'By default, your flows will be only shared with users you specify.',
+  },
+  {
+    name: FlowVisibility.HIDDEN,
+    icon: BanIcon,
+    description:
+      'Lone wolf. By default, your flows will not be shared with anyone else.',
+  },
 ]
 
 export default function InputDefaultVisibility() {
@@ -26,78 +41,106 @@ export default function InputDefaultVisibility() {
 
   if (!mounted) return null
 
-  console.log(userDetails)
-
   return (
     <div className="py-4 sm:py-5 relative">
       <dt className="text-sm text-info font-medium absolute left-0">
         Default Visibility
       </dt>
-      <Menu as="div" className="relative">
-        <Menu.Button
-          className={classNames(
-            {
-              'text-stone-500 hover:bg-gray-50 hover:border-gray-300':
-                theme === 'light',
-            },
-            {
-              'text-gray-400 hover:bg-slate-600 hover:border-slate-400':
-                theme === 'dark',
-            },
-            'ml-2 flex items-center font-light m-0 mr-2 text-lg px-2 hover:shadow-sm  border border-transparent  rounded-md cursor-pointer',
-          )}
-        >
-          <div>
-            {userDetails?.DefaultVisibility === FlowVisibility.PUBLIC && (
-              <RainbowPublicIcon dimension="w-5 h-5" />
-            )}
-            {userDetails?.DefaultVisibility === FlowVisibility.PRIVATE && (
-              <LockClosedIcon className="w-5 h-5" />
-            )}
-            {userDetails?.DefaultVisibility === FlowVisibility.HIDDEN && (
-              <BanIcon className="w-5 h-5" />
-            )}
-          </div>
-        </Menu.Button>
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-        >
-          <Menu.Items
+      <div className="flex justify-center w-full">
+        <Menu as="div" className="relative w-72">
+          <Menu.Button
             className={classNames(
               {
-                ' bg-white ring-black ring-1 ring-opacity-5': theme === 'light',
+                'text-stone-500 hover:bg-gray-50 hover:border-gray-300':
+                  theme === 'light',
               },
-              { 'bg-slate-700': theme === 'dark' },
-              'absolute z-40 left-[-2rem] origin-top-left rounded-md shadow-lg focus:outline-none',
+              {
+                'text-gray-400 hover:bg-slate-600 hover:border-slate-400':
+                  theme === 'dark',
+              },
+              ' flex items-center font-light m-0 text-lg px-2 hover:shadow-sm  border border-transparent  rounded-md cursor-pointer',
             )}
           >
-            {items.map((item) => (
-              <Menu.Item key={item.name}>
-                {({ active }: ActiveProps) => (
-                  <div
-                    className={classNames(
-                      { 'bg-gray-100': active && theme === 'light' },
-                      { 'bg-slate-600': active && theme === 'dark' },
-                      'pl-2 pr-4 py-2 text-sm first-of-type:rounded-t-md last-of-type:rounded-b-md flex items-center cursor-pointer',
-                    )}
-                    // onClick={() => mutator(item.name)}
-                    // onKeyDown={() => mutator(item.name)}
-                  >
-                    <item.icon className="w-4 mx-2" dimension="w-4 mx-2" />
-                    {item.name}
-                  </div>
-                )}
-              </Menu.Item>
-            ))}
-          </Menu.Items>
-        </Transition>
-      </Menu>
+            <div>
+              {userDetails?.DefaultVisibility === FlowVisibility.PUBLIC && (
+                <div className="text-sm flex flex-col items-center">
+                  <span className="font-medium">{items[0].name}</span>
+                  <RainbowPublicIcon dimension="w-5 h-5" />
+                  <span className="text-xs">{items[0].description} </span>
+                </div>
+              )}
+              {userDetails?.DefaultVisibility === FlowVisibility.PRIVATE && (
+                <div className="text-sm flex flex-col items-center">
+                  <span className="font-medium">{items[1].name}</span>
+                  <LockClosedIcon className="w-5 h-5" />
+                  <span className="text-xs">{items[1].description}</span>
+                </div>
+              )}
+              {userDetails?.DefaultVisibility === FlowVisibility.HIDDEN && (
+                <div className="text-sm flex flex-col items-center">
+                  <span className="font-medium">{items[2].name}</span>
+                  <BanIcon className="w-5 h-5" />
+                  <span className="text-xs">{items[2].description}</span>
+                </div>
+              )}
+            </div>
+          </Menu.Button>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items
+              className={classNames(
+                {
+                  ' bg-white ring-black ring-1 ring-opacity-5':
+                    theme === 'light',
+                },
+                { 'bg-slate-700': theme === 'dark' },
+                'absolute z-40 left-[-2rem] origin-top-left rounded-md shadow-lg focus:outline-none',
+              )}
+            >
+              {items.map((item) => (
+                <Menu.Item key={item.name}>
+                  {({ active }: ActiveProps) => (
+                    <div
+                      className={classNames(
+                        { 'bg-gray-100': active && theme === 'light' },
+                        { 'bg-slate-600': active && theme === 'dark' },
+                        'text-sm p-2 flex flex-col text-info items-center text-center cursor-pointer',
+                      )}
+                      onClick={() =>
+                        changeUserDefaultVisibility(
+                          item.name,
+                          userDetails,
+                          mutateUserDetails,
+                        )
+                      }
+                      onKeyDown={() =>
+                        changeUserDefaultVisibility(
+                          item.name,
+                          userDetails,
+                          mutateUserDetails,
+                        )
+                      }
+                    >
+                      <span className="font-medium">{item.name}</span>
+                      <item.icon className="w-4 mx-2" dimension="w-4 mx-2" />
+                      <span className="text-xs text-info">
+                        {item.description}
+                      </span>
+                    </div>
+                  )}
+                </Menu.Item>
+              ))}
+            </Menu.Items>
+          </Transition>
+        </Menu>
+      </div>
     </div>
   )
 }
