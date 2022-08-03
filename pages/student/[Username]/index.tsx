c65a48f2bcd2222eec79209faf3bfe6f2b26e180
alexjamesgodfrey <me@alexgodfrey.com>
2022-08-03 13:10:03 -0400

    Fix charles

--- pages/student/[Username]/index.tsx
+++ pages/student/[Username]/index.tsx
@@ -1,6 +1,8 @@
 import { useUser } from '@supabase/supabase-auth-helpers/react'
 import ProfileHeader from 'components/Social/Student/ProfileHeader'
 import ProfileTabs from 'components/Social/Student/ProfileTabs'
+import { DashFlow } from 'hooks/flows/useDashFlows'
+import get10PublicFlows from 'hooks/social/getPublicFlows'
 import getPublicProfile from 'hooks/social/getPublicProfile'
 import useUserDetails from 'hooks/useUserDetails'
 import { useEffect, useState } from 'react'
@@ -8,9 +10,10 @@ import { PublicUser } from 'types/Social'
 
 interface Props {
   PublicUser: PublicUser | undefined
+  PublicFlows: DashFlow[]
 }
 
-export default function index({ PublicUser }: Props) {
+export default function index({ PublicUser, PublicFlows }: Props) {
   // THE FOLLOWING IS ONLY USED TO GIVE THE USER ACCESS, DON'T USE IT TO DISPLAY DATA!
   const { user } = useUser()
   const { userDetails } = useUserDetails(user?.id)
@@ -24,6 +27,8 @@ export default function index({ PublicUser }: Props) {
   // END. PASS canEdit TO COMPONENTS THAT SHOULD BE EDITABLE
 
   if (!PublicUser) return <span>no user found!</span>
+
+  console.log(PublicFlows)
 
   return (
     <div className="h-full flex mb-5">
@@ -44,10 +49,14 @@ export default function index({ PublicUser }: Props) {
 export async function getServerSideProps(context: {
   query: { Username: string }
 }) {
-  const data = await getPublicProfile(context.query.Username)
+  const [publicUser, publicFlows] = await Promise.all([
+    getPublicProfile(context.query.Username),
+    get10PublicFlows(context.query.Username),
+  ])
   return {
     props: {
-      PublicUser: data,
+      PublicUser: publicUser,
+      PublicFlows: publicFlows,
     },
   }
 }
