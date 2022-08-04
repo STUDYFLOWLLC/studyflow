972fdd31ad432ce8dcbc359157eed570d955a93a
Alex Godfrey <alexjamesgodfrey@gmail.com>
2022-08-04 15:47:11 -0400

    Remove friend for yourself

--- components/Social/Student/ProfileHeader/FriendButton.tsx
+++ components/Social/Student/ProfileHeader/FriendButton.tsx
@@ -18,6 +18,8 @@ export default function FriendButton({ PublicUser }: Props) {
   const { userDetails } = useUserDetails(user?.id)
   const { friends, mutateFriends } = useFriends(userDetails?.UserID)
 
+  if (userDetails?.Username === PublicUser?.Username) return null
+
   if (!friends) return null
 
   if (
@@ -84,22 +86,26 @@ export default function FriendButton({ PublicUser }: Props) {
     )
   }
 
-  return (
-    <button
-      type="button"
-      className="inline-flex justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
-      onClick={() =>
-        sendFriendship(userDetails, PublicUser, friends, mutateFriends)
-      }
-      onKeyDown={() =>
-        sendFriendship(userDetails, PublicUser, friends, mutateFriends)
-      }
-    >
-      <UserAddIcon
-        className="-ml-1 mr-2 h-5 w-5 text-gray-400"
-        aria-hidden="true"
-      />
-      <span>Send Request</span>
-    </button>
-  )
+  if (userDetails && PublicUser) {
+    return (
+      <button
+        type="button"
+        className="inline-flex justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
+        onClick={() =>
+          sendFriendship(userDetails, PublicUser, friends, mutateFriends)
+        }
+        onKeyDown={() =>
+          sendFriendship(userDetails, PublicUser, friends, mutateFriends)
+        }
+      >
+        <UserAddIcon
+          className="-ml-1 mr-2 h-5 w-5 text-gray-400"
+          aria-hidden="true"
+        />
+        <span>Send Request</span>
+      </button>
+    )
+  }
+
+  return null
 }
