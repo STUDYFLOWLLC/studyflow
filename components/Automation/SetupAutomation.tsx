f060347957881392287e65bb5c842214aea162d3
alexjamesgodfrey <me@alexgodfrey.com>
2022-08-04 22:06:07 -0400

    Automation wording and darkify

--- components/Automation/SetupAutomation.tsx
+++ components/Automation/SetupAutomation.tsx
@@ -21,23 +21,23 @@ const features = [
   {
     name: '1. Connect to Google Drive',
     description:
-      'Easily connect your Google Drive and/or Notability/Goodnotes to get started.',
+      'Turn on automatic backup in your favorite note-taking app (or take notes in Google Docs or upload paper notes to Google Drive!)',
     icon: LinkIcon,
   },
   {
     name: '2. Make a note in Notability or GoodNotes',
     description:
-      'During a usual lecture, take a note and then it will automatically back up to Google Drive and it will notify our platform.',
+      'Our servers will watch your Google Drive for new files in the folders you specify. Take a a note in class, and let Studyflow do the rest.',
     icon: ScaleIcon,
   },
   {
     name: '3. Let Studyflow do the rest',
     description:
-      'Studyflow will automatically create a flow and flashcard stack with review-tasks one day, three days, one week, and one month after the creation.',
+      'Studyflow will automatically create a flow, flashcard stack, and a science-based review schedule for you.',
     icon: LightningBoltIcon,
   },
   {
-    name: '4. Review on our automated review schedule',
+    name: '4. Learn with no friction',
     description:
       'Review your flashcard stacks when Studyflow notifies you, and never have a problem remembering content again.',
     icon: AnnotationIcon,
@@ -122,30 +122,32 @@ export default function SetupAutomation() {
           Connect Google Drive
           <ButtonSpinner show={loading} />
         </button> */}
-      <div className="pb-12 pt-6 bg-white ml-56 mr-6">
+      <div className="pb-12 pt-6 bg-base-100 lg:ml-56 mr-6 relative">
+        <button
+          disabled={loading}
+          type="button"
+          className="alex-button px-4 py-3 font-semibold rounded-md shadow-sm uppercase absolute top-2 md:top-4 right-0"
+          onClick={async () => {
+            setLoading(true)
+            login()
+          }}
+        >
+          Get Started
+          <ButtonSpinner show={loading} />
+        </button>
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="lg:text-center">
-            <div className="flex justify-end -mb-6">
-              <button
-                disabled={loading}
-                type="button"
-                className="flex items-center justify-center bg-primary/80 hover:bg-primary bg-origin-border px-4 py-3 border border-transparent text-xl font-semibold rounded-md shadow-sm text-black"
-                onClick={async () => {
-                  setLoading(true)
-                  login()
-                }}
-              >
-                GET STARTED
-                <ButtonSpinner show={loading} />
-              </button>
-            </div>
             <h2 className="text-xl text-primary font-semibold tracking-wide uppercase">
               Automation
             </h2>
-            <div className="mt-4 text-4xl leading-8 font-extrabold tracking-tight text-gray-900">
-              Automate your workflow
+            <div className="mt-4 text-4xl leading-8 font-extrabold tracking-tight">
+              Automate your{' '}
+              <span className="line-through decoration-4 mr-2">workflow</span>
+              <span className="underline decoration-primary decoration-4">
+                studyflow
+              </span>
             </div>
-            <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto pt-4 pb-6">
+            <p className="mt-4 max-w-2xl text-xl text-info lg:mx-auto pt-4 pb-6">
               Never have to think about what and when to study again with our
               science-proven study automator. Here's how it works:
             </p>
@@ -159,11 +161,11 @@ export default function SetupAutomation() {
                     <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-primary text-white">
                       <feature.icon className="h-6 w-6" aria-hidden="true" />
                     </div>
-                    <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
+                    <p className="ml-16 text-lg leading-6 font-medium">
                       {feature.name}
                     </p>
                   </dt>
-                  <dd className="mt-2 ml-16 text-base text-gray-500">
+                  <dd className="mt-2 ml-16 text-base text-info">
                     {feature.description}
                   </dd>
                 </div>
